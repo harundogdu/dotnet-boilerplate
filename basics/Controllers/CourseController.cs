d3cbda62c99c4bde7f32596055f9245ff02fd989
Harun Doğdu <harundogdu06@gmail.com>
2025-01-18 21:25:23 +0300

    Add CourseController and solution files for basics project

---
+++ basics/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+public class CourseController : Controller
+{
+    public string Index()
+    {
+        return "course/index";
+    }
+
+    public string List()
+    {
+        return "course/list";
+    }
+}
