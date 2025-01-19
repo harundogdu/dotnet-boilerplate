5c111a547ca44123e1e48c475054ab88572550ee
Harun Doğdu <harundogdu06@gmail.com>
2025-01-19 22:47:22 +0300

    Add course descriptions and update List action to return a list of courses

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -11,11 +11,18 @@ public class CourseController : Controller
         var course = new Course();
         course.Id = 1;
         course.Title = "ASP.NET Core MVC";
+        course.Description = "Learn ASP.NET Core MVC from scratch.";
         return View(course);
     }
 
     public IActionResult List()
     {
-        return View("CourseList");
+        var courses = new List<Course>(){
+            new Course{Id = 1, Title = "ASP.NET Core MVC", Description = "Learn ASP.NET Core MVC from scratch."},
+            new Course{Id = 2, Title = "ASP.NET Core Web API", Description = "Learn ASP.NET Core Web API from scratch."},
+            new Course{Id = 3, Title = "ASP.NET Core Blazor", Description = "Learn ASP.NET Core Blazor from scratch."},
+            new Course{Id = 4, Title = "ASP.NET Core SignalR", Description = "Learn ASP.NET Core SignalR from scratch."},
+        };
+        return View("CourseList", courses);
     }
 }
