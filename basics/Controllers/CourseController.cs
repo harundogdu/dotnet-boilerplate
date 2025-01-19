405f231d3ec72b86e6f7377a3bfea3cfffc4da25
Harun Doğdu <harundogdu06@gmail.com>
2025-01-19 23:18:13 +0300

    feat: add image property to courses in CourseController

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -12,16 +12,17 @@ public class CourseController : Controller
         course.Id = 1;
         course.Title = "ASP.NET Core MVC";
         course.Description = "Learn ASP.NET Core MVC from scratch.";
+        course.Image = "1.jpg";
         return View(course);
     }
 
     public IActionResult List()
     {
         var courses = new List<Course>(){
-            new Course{Id = 1, Title = "ASP.NET Core MVC", Description = "Learn ASP.NET Core MVC from scratch."},
-            new Course{Id = 2, Title = "ASP.NET Core Web API", Description = "Learn ASP.NET Core Web API from scratch."},
-            new Course{Id = 3, Title = "ASP.NET Core Blazor", Description = "Learn ASP.NET Core Blazor from scratch."},
-            new Course{Id = 4, Title = "ASP.NET Core SignalR", Description = "Learn ASP.NET Core SignalR from scratch."},
+            new Course{Id = 1, Title = "ASP.NET Core MVC", Description = "Learn ASP.NET Core MVC from scratch.", Image = "1.jpg"},
+            new Course{Id = 2, Title = "ASP.NET Core Web API", Description = "Learn ASP.NET Core Web API from scratch.", Image = "2.jpg"},
+            new Course{Id = 3, Title = "ASP.NET Core Blazor", Description = "Learn ASP.NET Core Blazor from scratch.", Image = "3.jpg"},
+            new Course{Id = 4, Title = "ASP.NET Core SignalR", Description = "Learn ASP.NET Core SignalR from scratch.", Image = "4.jpg"},
         };
         return View("CourseList", courses);
     }
