9f94f134d0ebbc4f8830da8e1e86d21244307296
Harun Doğdu <harundogdu06@gmail.com>
2025-01-20 22:29:56 +0300

    feat: refactor CourseController List action to use Repository for course data

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -18,13 +18,7 @@ public class CourseController : Controller
 
     public IActionResult List()
     {
-        var courses = new List<Course>(){
-            new Course{Id = 1, Title = "ASP.NET Core MVC", Description = "Learn ASP.NET Core MVC from scratch.", Image = "1.jpg"},
-            new Course{Id = 2, Title = "ASP.NET Core Web API", Description = "Learn ASP.NET Core Web API from scratch.", Image = "2.jpg"},
-            new Course{Id = 3, Title = "ASP.NET Core Blazor", Description = "Learn ASP.NET Core Blazor from scratch.", Image = "3.jpg"},
-            new Course{Id = 4, Title = "ASP.NET Core SignalR", Description = "Learn ASP.NET Core SignalR from scratch.", Image = "4.jpg"},
-        };
-        return View("CourseList", courses);
+        return View("CourseList", Repository.Courses);
     }
 
     public IActionResult Details()
