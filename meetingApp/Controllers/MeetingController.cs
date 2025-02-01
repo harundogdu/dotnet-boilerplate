b8a146a5923f81213d7330a8d4f97b3116f1e7c0
Harun Doğdu <harundogdu06@gmail.com>
2025-02-01 12:00:00 +0300

    feat: add HTTP GET actions for Apply, List, and Detail views in MeetingController

--- meetingApp/Controllers/MeetingController.cs
+++ meetingApp/Controllers/MeetingController.cs
@@ -4,18 +4,25 @@ namespace MeetingApp.Controllers
 {
     public class MeetingController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult List()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult Detail()
         {
             return View();
