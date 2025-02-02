82c83ad32d752380b1bd2fdd71569c5f26925035
Harun Doğdu <harundogdu06@gmail.com>
2025-02-02 12:00:00 +0300

    feat: add UserInfo model to store user details for meeting applications

---
+++ meetingApp/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace meetingApp.Models;
+
+public class UserInfo
+{
+    public string? Name { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public bool WillAttend { get; set; }
+}
