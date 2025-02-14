648ae273ecbee7d880d723efe909d120ad0c2a80
Harun Doğdu <harundogdu06@gmail.com>
2025-02-14 12:00:00 +0300

    feat: initialize user data in Repository class with sample attendees

--- meetingApp/Models/Repository.cs
+++ meetingApp/Models/Repository.cs
@@ -7,6 +7,31 @@ public static class Repository
 {
     public static List<UserInfo> _users = new();
 
+    static Repository()
+    {
+        _users.Add(new UserInfo()
+        {
+            Name = "Harun Doğdu",
+            Email = "harundogdu@hd.com",
+            Phone = "1234567890",
+            WillAttend = true
+        });
+        _users.Add(new UserInfo()
+        {
+            Name = "Muhammed Emin Beköz",
+            Email = "mami@hd.com",
+            Phone = "1451567890",
+            WillAttend = true
+        });
+        _users.Add(new UserInfo()
+        {
+            Name = "Ahmet Enes Bahar",
+            Email = "amedenez@hd.com",
+            Phone = "1451567880",
+            WillAttend = true
+        });
+    }
+
     public static List<UserInfo> Users
     {
         get
