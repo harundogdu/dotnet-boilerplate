d9ecf6c1dc06f622e3996609cd70ad504701af4a
Harun Doğdu <harundogdu06@gmail.com>
2025-03-11 22:18:11 +0300

    feat: add data annotations for Product model properties to enhance display names

--- FormsApp/Models/Product.cs
+++ FormsApp/Models/Product.cs
@@ -1,12 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FormsApp.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+        [Display(Name = "Product name")]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        [Display(Name = "Image")]
         public string ImageUrl { get; set; } = string.Empty;
+        [Display(Name = "Is available")]
         public bool IsAvailable { get; set; }
         public int CategoryId { get; set; }
     }
