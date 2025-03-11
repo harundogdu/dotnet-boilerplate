ec86dd2b02649a21c697bb8805d1890e0e2f5b42
Harun Doğdu <harundogdu06@gmail.com>
2025-03-11 23:04:25 +0300

    feat: add category filtering to Index action in HomeController

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace FormsApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(string searchString)
+        public IActionResult Index(string? searchString, string? categoryId)
         {
             var products = Repository.Products;
 
@@ -15,8 +15,13 @@ namespace FormsApp.Controllers
                 products = products.FindAll(p => p.Name.Contains(searchString, StringComparison.CurrentCultureIgnoreCase));
             }
 
+            if (!string.IsNullOrEmpty(categoryId) && categoryId != "0")
+            {
+                products = [.. products.Where(p => p.CategoryId == int.Parse(categoryId))];
+            }
+
             ViewBag.SearchString = searchString;
-            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name"); // açıklama: SelectList sınıfı, bir veri koleksiyonunu bir DropDownList veya ListBox denetimine bağlamak için kullanılır. İlk parametre, veri koleksiyonunu, ikinci parametre, DropDownList veya ListBox denetiminde görünecek metin ve üçüncü parametre, DropDownList veya ListBox denetiminde görünecek değerdir.
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", categoryId); // açıklama: SelectList sınıfı, bir veri koleksiyonunu bir DropDownList veya ListBox denetimine bağlamak için kullanılır. İlk parametre, veri koleksiyonunu, ikinci parametre, DropDownList veya ListBox denetiminde görünecek metin ve üçüncü parametre, DropDownList veya ListBox denetiminde görünecek değerdir.
 
             return View(products);
         }
