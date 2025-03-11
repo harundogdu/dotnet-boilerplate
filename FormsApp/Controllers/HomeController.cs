b69a66975c82257f66b619de1b0dd905c73ca529
Harun Doğdu <harundogdu06@gmail.com>
2025-03-11 20:51:51 +0300

    feat: initialize ASP.NET Core project structure with HomeController and basic layout

---
+++ FormsApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
