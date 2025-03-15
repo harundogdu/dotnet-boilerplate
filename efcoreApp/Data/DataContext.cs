23366aa1cd5e208709cf57ccc0c769e5f35eff8f
Harun Doğdu <harundogdu06@gmail.com>
2025-03-15 15:08:05 +0300

    feat: add DataContext for managing database entities

---
+++ efcoreApp/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Course> Courses => Set<Course>();
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<CourseEnrollment> CourseEnrollments => Set<CourseEnrollment>();
+    }
+}
