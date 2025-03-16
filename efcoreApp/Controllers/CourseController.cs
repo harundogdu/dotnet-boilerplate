99ccfa207ad474cb83c7d2b45f1f72a060662686
Harun Doğdu <harundogdu06@gmail.com>
2025-03-16 18:38:11 +0300

    feat: add Edit action to CourseController for course editing functionality

--- efcoreApp/Controllers/CourseController.cs
+++ efcoreApp/Controllers/CourseController.cs
@@ -44,5 +44,48 @@ namespace efcoreApp.Controllers
             return View(course);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            var course = _context.Courses.Find(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(int id, Course model)
+        {
+            var course = _context.Courses.Find(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    course.CourseTitle = model.CourseTitle;
+                    _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "An error occurred while updating the course.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+
+        }
+
     }
 }
