b6c1f49389efe8935d1bcd1bcc38bd3e52aa0dcb
Harun Doğdu <harundogdu06@gmail.com>
2025-03-16 18:27:13 +0300

    feat: add validation attributes to CourseTitle property in Course model

--- efcoreApp/Data/Course.cs
+++ efcoreApp/Data/Course.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace efcoreApp.Data
 {
     public class Course
     {
         public int CourseId { get; set; }
+
+        [Required]
+        [Display(Name = "Course title")]
         public string? CourseTitle { get; set; }
     }
 }
