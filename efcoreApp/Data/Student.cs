1b54104acb3a09d88b42c8ee8617a5fe29fb18ec
Harun Doğdu <harundogdu06@gmail.com>
2025-03-16 16:36:41 +0300

    feat: add validation attributes to Student model properties

--- efcoreApp/Data/Student.cs
+++ efcoreApp/Data/Student.cs
@@ -6,10 +6,22 @@ namespace efcoreApp.Data
     {
         [Key] // Primary Key: Herhangi bir key'in primary key olduğunu belirtir. "classAdıId" veya "Id" olarak yazılırsa bunun eklenmesine gerek yoktur.
         public int StudentId { get; set; }
+
+        [Required]
         public string? FirstName { get; set; }
+
+        [Required]
         public string? LastName { get; set; }
+
+        [Required]
+        [Phone]
         public string? PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Required]
         public DateTime BirthTime { get; set; }
     }
 }
